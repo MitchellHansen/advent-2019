a912d58e6fa9f2c792ca8748b165b2d0dcdba55b
mitchellhansen <mitchellhansen0@gmail.com>
2019-12-01 20:56:12 -0800

    .

--- src/util.rs
+++ src/util.rs
@@ -9,10 +9,11 @@ use reqwest::cookie::Cookie;
 use reqwest::header::HeaderValue;
 use std::fs;
 use std::path::Path;
+use std::env;
 
 fn get_body(problem: i32) -> String {
 
-    fs::create_dir("./cache").expect("");
+    fs::create_dir_all("./cache").expect("");
 
     let filepath = format!("./cache/problem-{}", problem);
     let url = format!("https://adventofcode.com/2019/day/{}/input", problem);
@@ -20,13 +21,21 @@ fn get_body(problem: i32) -> String {
     if Path::new(&filepath).exists() {
         fs::read_to_string(filepath).unwrap()
     } else {
+
+        let session_token = match env::var("SESSION") {
+            Ok(token) => token,
+            Err(e) => panic!("No session token"),
+        };
+
+        let session_token = format!("session={};", session_token);
+
         let client = reqwest::Client::new();
         let body = client
             .get(&url)
-            .header("Cookie", "session=53616c7465645f5f9a1fd404211ee5731f6b070241a384279cd0ac5ea569fb1c5ba9f009f866c493e23670d4042b1167;")
+            .header("Cookie", session_token)
             .send().expect("").text().expect("");
 
-        let mut f = File::create(format!("./problem-{}", problem)).expect("");
+        let mut f = File::create(format!("./cache/problem-{}", problem)).expect("");
         f.write_all(body.as_bytes());
 
         body
